e5cb0d5356027c100a67e2c7398657601c6245e5
Niro Liyanage <niro.liyanage@touchcorp.com>
2019-12-14 03:03:25 +1100

    add v1.3

---
+++ v1.3/deployment/build-s3-dist.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# This assumes all of the OS-level configuration has been completed and git repo has already been cloned
+#
+# This script should be run from the repo's deployment directory
+# cd deployment
+# ./build-s3-dist.sh source-bucket-base-name trademarked-solution-name version-code
+#
+# Parameters:
+#  - source-bucket-base-name: Name for the S3 bucket location where the template will source the Lambda
+#    code from. The template will append '-[region_name]' to this bucket name.
+#    For example: ./build-s3-dist.sh solutions my-solution v1.0.0
+#    The template will then expect the source code to be located in the solutions-[region_name] bucket
+#
+#  - trademarked-solution-name: name of the solution for consistency
+#
+#  - version-code: version of the solution
+
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "Please provide the base source bucket name, trademark approved solution name and version where the lambda code will eventually reside."
+    echo "For example: ./build-s3-dist.sh solutions trademarked-solution-name v1.0.0"
+    exit 1
+fi
+
+echo "make bucket=$1 solution=$2 version=$3"
+cd ../source/code
+make bucket=$1 solution=$2 version=$3
+cd ../../deployment
+echo "Completed building distribution"
